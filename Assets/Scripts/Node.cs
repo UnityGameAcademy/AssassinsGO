e449df6b245fb47a52b5033affb68fd2311890f6
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 11:27:14 -0700

    Neighbor Nodes (Part 2)

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+	// (x,z) coordinate on Board, Coordinate property always returns rounded Vector
+	Vector2 m_coordinate;
+    public Vector2 Coordinate { get { return Utility.Vector2Round(m_coordinate); } }
+
+	// list of nodes that are adjacent based on Board spacing
+	List<Node> m_neighborNodes = new List<Node>();
+    public List<Node> NeighborNodes { get { return m_neighborNodes; } }
+
+	// reference to the Board component
+	Board m_board;
+
     // reference to mesh for display of the node
     public GameObject geometry;
 
@@ -19,6 +30,15 @@ public class Node : MonoBehaviour
     // delay time before animation
     public float delay = 1f;
 
+    void Awake()
+    {
+		// find reference to the Board component
+		m_board = Object.FindObjectOfType<Board>();
+
+		// set the coordinate using the transform's x and z values
+		m_coordinate = new Vector2(transform.position.x, transform.position.z);
+    }
+
     void Start()
     {
         // hide the mesh by scaling to zero
@@ -30,6 +50,12 @@ public class Node : MonoBehaviour
             if (autoRun)
             {
                 ShowGeometry();
+            }
+
+			// find the neighboring nodes
+			if (m_board != null)
+            {
+                m_neighborNodes = FindNeighbors(m_board.AllNodes);
             }
         }
     }
@@ -48,4 +74,25 @@ public class Node : MonoBehaviour
         }
     }
 
+	// given a list of Nodes, return a subset of the list that are neighbors
+	public List<Node> FindNeighbors(List<Node> nodes)
+    {
+		// temporary list of nodes to return
+		List<Node> nList = new List<Node>();
+
+		// loop through each of the Board directions
+		foreach (Vector2 dir in Board.directions)
+        {
+			// find a neighboring node at the current direction...
+			Node foundNeighbor = nodes.Find(n => n.Coordinate == Coordinate + dir);
+
+			// if we find a neighbor at this direction, add it to the list
+			if (foundNeighbor != null && !nList.Contains(foundNeighbor))
+            {
+                nList.Add(foundNeighbor);
+            }
+        }
+		// return our temporary list
+		return nList;
+    }
 }
