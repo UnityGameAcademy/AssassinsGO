9efecf101d0ab9d9bb336deca49f84e3013a711d
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 09:53:10 -0700

    PlayerInput Properties

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     // store horizontal input
-    public float h;
+    float m_h;
+    public float H { get { return m_h; } }
 
     // store the vertical input 
-    public float v;
+    float m_v;
+    public float V { get { return m_v; } }
 
     // global flag for enabling and disabling user input
-    public bool inputEnabled = false;
+    bool m_inputEnabled = false;
+    public bool InputEnabled { get { return m_inputEnabled; } set { m_inputEnabled = value; } }
+
+    // playerInput.InputEnabled = true;
 
     // get keyboard input
     public void GetKeyInput()
     {
         // if input is enabled, just get the raw axis data from the Horizontal and Vertical virtual axes (defined in InputManager)
-        if (inputEnabled)
+        if (m_inputEnabled)
         {
-			h = Input.GetAxisRaw("Horizontal");
-			v = Input.GetAxisRaw("Vertical");  
+            m_h = Input.GetAxisRaw("Horizontal");
+            m_v = Input.GetAxisRaw("Vertical");
         }
     }
+
 }
