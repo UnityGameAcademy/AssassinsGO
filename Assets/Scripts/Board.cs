bafa91f099779b7f9ab6d4076ce7a97270326536
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 10:09:05 -0700

    The Game Board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // uniform distance between nodes
+    public static float spacing = 2f;
+
+    // four compass directions
+    public static readonly Vector2[] directions =
+    {
+        new Vector2(spacing, 0f),
+        new Vector2(-spacing, 0f),
+        new Vector2(0f, spacing),
+        new Vector2(0f, -spacing)
+    };
+}
