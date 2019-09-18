15b171fdd64a7eaa281e6c912a1ba6a482283bee
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 11:05:55 -0700

    Nodes (with updates to PostProcessing version 2)

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // reference to mesh for display of the node
+    public GameObject geometry;
+
+    // time for scale animation to play
+    public float scaleTime = 0.3f;
+
+    // ease in-out for animation
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+
+    // do we activate the animation at Start?
+    public bool autoRun = false;
+
+    // delay time before animation
+    public float delay = 1f;
+
+    void Start()
+    {
+        // hide the mesh by scaling to zero
+        if (geometry != null)
+        {
+            geometry.transform.localScale = Vector3.zero;
+
+            // play scale animation at Start
+            if (autoRun)
+            {
+                ShowGeometry();
+            }
+        }
+    }
+
+    // play scale animation
+    public void ShowGeometry()
+    {
+        if (geometry != null)
+        {
+            iTween.ScaleTo(geometry, iTween.Hash(
+                "time", scaleTime,
+                "scale", Vector3.one,
+                "easetype", easeType,
+                "delay", delay
+            ));
+        }
+    }
+
+}
