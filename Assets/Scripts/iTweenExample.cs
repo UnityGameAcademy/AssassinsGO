49bdea9131281c02005215f143b4ff29e0e1bf2d
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 08:25:32 -0700

    201 Project Setup and iTween

---
+++ Assets/Scripts/iTweenExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iTweenExample : MonoBehaviour
+{
+
+    void Start()
+    {
+        iTween.RotateTo(gameObject, iTween.Hash(
+            "y", 90f,
+            "delay", 1f,
+            "time", 2f,
+            "easetype", iTween.EaseType.easeInOutExpo));
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AssassinsGO
 updated Make an Assassins GO game for Unity 2019
+
+These project files accompany the coursework: 
+https://www.udemy.com/course/make-an-assassins-go-board-game-in-unity/
+
+These  files update the original Unity 2017 project to Unity 2019.2f.
+
