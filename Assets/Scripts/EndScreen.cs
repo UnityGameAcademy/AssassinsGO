3525d1241b1a9b12edc6c6643ed0c32224a17df9
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-18 18:29:01 -0700

    End Screen Blur - updated to version 2 PostProcessing with Unity 2019.2

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class EndScreen : MonoBehaviour
+{
+
+    public PostProcessVolume postProcessVolume;
+
+    public PostProcessProfile blurProfile;
+    public PostProcessProfile normalProfile;
+
+    // switch between the blurProfile and normalProfile
+    public void EnableCameraBlur(bool state)
+    {
+        if (postProcessVolume != null && blurProfile != null && normalProfile != null)
+        {
+            postProcessVolume.profile = (state) ? blurProfile : normalProfile;
+        }
+    }
+}
