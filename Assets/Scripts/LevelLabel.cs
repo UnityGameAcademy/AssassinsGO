31a75d44e5d06180da045e4772d7f51956113421
Wilmer Lin <unitygameacademy@gmail.com>
2019-09-21 23:38:28 -0700

    Level Label

---
+++ Assets/Scripts/LevelLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LevelLabel : MonoBehaviour
+{
+
+    Text m_text;
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+
+        if (m_text != null)
+        {
+            m_text.text = SceneManager.GetActiveScene().name;
+        }
+    }
+
+}
